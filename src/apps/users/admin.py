28082c0ccd701cd45fa221b26800fd555bb8b43e
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-22 23:39:20 +0200

    mod admin site

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -1,7 +1,43 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
 
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class UserAdminCustom(UserAdmin):
+    
+    list_display = ("email", "pk", "username", "profile", "is_staff")
+    list_filter = ("created_on", "last_login", "is_staff", "is_active")
+    list_display_links = ("email", "pk")
+    list_editable = ("is_staff", )
+    search_fields = ("username__startswith", )
+    ordering = ("pk", )
+    readonly_fields = ("slug", )
+    # add show user avatar
+    
+    def profile(self, obj):
+        link = reverse("profile_detail_view", args=(obj.slug, ))
+        return format_html(f"<a href='{link}'>link</a>")
+    
+
+    fieldsets = (
+        (_("Register info"), {"fields": ("username", "email", "password", )}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "slug", "avatar")}),
+        (
+            _("Permissions"),
+            {
+                "classes": ["collapse"],
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Dates"), {"fields": ("last_login", "date_joined")}),
+    )
