9e2fab471d7bebf008f56925188a52d37aaec4f2
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-27 10:15:19 +0200

    add paginate

--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -6,6 +6,6 @@ from apps.posts.models import Post
 class HomePageView(generic.ListView):
     model = Post
     template_name = "core/index.html"
-    paginate_by = 5
+    paginate_by = 2
     context_object_name = "posts"
     ordering = ("-created_on", )
