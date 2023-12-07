c0fc7728171f5cef94f05764879695069f89750a
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-07 20:48:34 +0200

    mod shows auth with social network

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -2,6 +2,8 @@ from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 
+from allauth.socialaccount.models import SocialApp
+
 from .mixins import LoginPermissionMixin
 from . import forms
 from .models import User
@@ -9,6 +11,7 @@ from .models import User
 
 class UserSingUpView(CreateView):
     model = User
+    extra_context = {"social": SocialApp.objects.all()}
     template_name = "users/signup.html"
     form_class = forms.UserSignUpForm
     success_url = reverse_lazy("users:login_view")
@@ -16,6 +19,7 @@ class UserSingUpView(CreateView):
 
 class UserLogInView(LoginView):
     template_name = "users/login.html"
+    extra_context = {"social": SocialApp.objects.all()}
     authentication_form = forms.UserLogInForm
     redirect_authenticated_user = True
     next_page = reverse_lazy("core:index_view")
