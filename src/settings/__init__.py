9fd686fda05ea09f1ea03e2b704377ccd5fb271b
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-15 21:48:44 +0200

    update TODO

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -1,18 +1,16 @@
 # TODO: write handle database erro and all 500 error
 # TODO: write templates to error page
 
-# TODO: add autocomplete to search text
-
 # TODO: rewrite django-allauth templates to custom
 
-# TODO: add ability change password
+# TODO: add Contact page
+
+# TODO: add caching
 
 # TODO: write comment and like system
 
 # TODO: add favorites posts and subscriptions
 
 # TODO: move logic from view to signals
-# TODO: wrap queries innto transactions
-# TODO: optimize sql queries
 
-# TODO: add caching
+# TODO: add autocomplete to search text
