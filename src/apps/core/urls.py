44e9fcb56ef206ea9ce72769836192f4d00065b1
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-15 21:50:16 +0200

    created view for contacts page

--- src/apps/core/urls.py
+++ src/apps/core/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 app_name = "core"
 
-urlpatterns = [path(route="", view=views.HomePageView.as_view(), name="index_view")]
+urlpatterns = [
+    path("", view=views.HomePageView.as_view(), name="index_view"),
+    path("/contacts", view=views.ContactPageView.as_view(), name="contacts_view")
+    ]
--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -1,4 +1,6 @@
 from django.views import generic
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
 from apps.posts.models import Post
 
@@ -18,3 +20,37 @@ class HomePageView(generic.ListView):
     paginate_by = 5
     context_object_name = "posts"
     ordering = ("-created_on",)
+
+
+class ContactPageView(generic.TemplateView):
+    """
+    View for contacts page it contans informations
+    about website author; him contacts, locations, and name
+    """
+    template_name = "core/contacts.html"
+    extra_context = {
+        "name": _("Ilya Bulatau"),
+        "about": _("""
+            Hi, I'm a Python developer with 2 years of experience. 
+            I designed this site as my portfolio. 
+            On this site you can write and publish your articles, 
+            read articles written by other people. 
+            You can also update your profile. 
+            On this page you will find information on how to contact me.
+            """),
+        "CV": settings.RESUME_PATH,
+        "socials": [
+            {
+                "link": "https://t.me/ilbltv",
+                "image": "https://img.shields.io/badge/Telegram--blue?labelColor=green"
+            },
+            {
+                "link": "https://www.linkedin.com/in/ilya-bulatau-585133253",
+                "image": "https://img.shields.io/badge/LinkedIn--blue?labelColor=green"
+            },
+            {
+                "link": "https://github.com/IlyaBulatau",
+                "image": "https://img.shields.io/badge/GitHub--blue?labelColor=green"
+            },
+        ]
+    }
