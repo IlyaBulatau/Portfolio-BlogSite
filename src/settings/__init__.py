96e649a1a2689eed95392606b75c38f510b329d9
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-17 10:57:00 +0200

    Mod TODOs

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -1,13 +1,11 @@
+# TODO: rewrite django-allauth templates to custom
+
+# TODO: write comment and like system
+
 # TODO: write handle database erro and all 500 error
 # TODO: write templates to error page
 
-# TODO: rewrite django-allauth templates to custom
-
-# TODO: add Contact page
-
 # TODO: add caching
-
-# TODO: write comment and like system
 
 # TODO: add favorites posts and subscriptions
 
