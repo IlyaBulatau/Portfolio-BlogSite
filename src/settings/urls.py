e827bea02fcfcccbfa54e1f1103553568ff26d4b
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-21 18:50:40 +0200

    mod django admin app registration

--- src/settings/urls.py
+++ src/settings/urls.py
@@ -14,17 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
 
-admin_site = [path(f"{site.name}/", site.urls) for site in admin.sites.all_sites]
-
 urlpatterns = [
-    *admin_site,
+    path(f"{settings.ADMIN_SITE_URL}/", admin.site.urls),
     
     path("", include("apps.core.urls")),
     path("users/", include("apps.users.urls")),
