7b155e2dbeef2bcf8f6cbfd18251a1d5e8e0a304
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-21 18:52:26 +0200

    wrote AdminPageMiddleware for limit access to django admin panel

---
+++ src/settings/middlewares.py
@@ -0,0 +1,22 @@
+from django.http.request import HttpRequest
+from django.core.exceptions import PermissionDenied
+from django.conf import settings
+
+class AdminPageMiddleware:
+    """
+    Allows access only for auth admin or client with ADMIN_IP address
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest):
+        admin_ip = request.META.get("REMOTE_ADDR")
+        if (
+            request.path.startswith(f"/{settings.ADMIN_SITE_URL}")
+            and 
+            admin_ip != settings.ADMIN_IP
+            ):
+            if not request.user.is_staff:
+                raise PermissionDenied()
+        return self.get_response(request)
