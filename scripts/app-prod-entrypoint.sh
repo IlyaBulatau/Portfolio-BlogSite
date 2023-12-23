5659f288d2db9932116a85de2403b4cd03eebaee
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-23 19:21:27 +0200

    mod prod script file moved set environments to .env.prod file and compose prod file

--- scripts/app-prod-entrypoint.sh
+++ scripts/app-prod-entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-export MODE='prod'
-export PYTHONPATH=$(pwd)/src
-
-export $(grep -v '^#' ./env/.env.prod | xargs)
 
 poetry run python3 src/manage.py migrate
 poetry run gunicorn src.settings.wsgi --bind 0.0.0.0:8000
