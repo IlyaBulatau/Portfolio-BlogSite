f5be62bbe78564e0ce32ae8fee2018fa3364c528
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-23 21:58:54 +0200

    updated gunicorn run added log-level

--- scripts/app-prod-entrypoint.sh
+++ scripts/app-prod-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 poetry run python3 src/manage.py migrate
-poetry run gunicorn src.settings.wsgi --bind 0.0.0.0:8000
+poetry run gunicorn src.settings.wsgi --bind 0.0.0.0:80 --log-level debug 
