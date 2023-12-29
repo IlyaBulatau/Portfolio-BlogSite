58e2e7d81b82cecddd4466047e0fc9ddaadbd754
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-29 21:17:03 +0200

    changed domains address

--- deploy/init-letsencrypt.sh
+++ deploy/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(ilyabulatau-portfolio.ru ilyabulatau-portfolio.store)
+domains=(ilyabulatau-portfolio.ru www.ilyabulatau-portfolio.ru)
 rsa_key_size=4096
 data_path="./deploy/certbot"
 email="ilyabulatau@gmail.com" 
